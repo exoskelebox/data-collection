bc10551cec867b1870e5928bd4f8b8d9c6ec3c98
BullDuckling <tbuhla@gmail.com>
2020-02-19 13:49:10 +0100

    async testing

--- sensors.py
+++ sensors.py
@@ -3,8 +3,17 @@ import array
 import datetime
 import sys
 import time
-from data_collection import biox_devices
+#from data_collection import biox_devices
+# Testing stuff, remove when done
 import timeit
+import random
+import asyncio
+from joblib import Parallel, delayed
+
+biox_devices = {
+    'wrist': '4407090',
+    'arm': '4083140'
+}
 
 
 
@@ -104,53 +113,8 @@ def calibrate_sensor(port, num_to_max=2):
             serial.close()
 
 
-def old_collect_sensor_data(ports, data_stream):
-    """Collects 10 data readings from the given sensor ports and returns lists for each wristband values and timestamp.
-
-    >>> collect_sensor_data([]) is None
-    True
-    """
-    if not len(ports) > 0:
-        return None
-
-    sensors_amount = 8
-    repetition = 10
-    # How many readings for each sensor do we want to receive from the device
-    data_size = sensors_amount * repetition
-    wrist_data = []
-    forearm_data = []
-
-    t = datetime.datetime.utcnow()
-
-
-    for port in ports:
-        #port.write('S'.encode())
-        stream = array.array("B")
-        #data_stream.fromfile(port, data_size)
-        i = 1
-        temp = []
-
-        while i < len(data_stream):
-            j = i + sensors_amount
-            while i < j:
-                i += 1
-                temp.append(data_stream[len(data_stream) - i])
-            
-        if port == "COM4":
-            # Sensor device with 7 FSR
-            # Pop index 1 as we only have 7 FSR and that value is not doing anything.
-            temp.pop(6)
-            wrist_data = temp
-        elif port == "COM3":
-            # Sensor device with 8 FSR
-            forearm_data = temp
-        else:
-            sys.stdout.write('ERROR: Collect_sensor_data: unknown device')
-    return wrist_data, forearm_data, t
-
-
 def collect_sensor_data(ports, data_stream):
-    """Collects 10 data readings from the given sensor ports and returns lists for each wristband values and timestamp.
+    """Collects a data reading from the given sensor ports and returns lists for each wristband values and timestamp.
 
     >>> collect_sensor_data([]) is None
     True
@@ -160,6 +124,7 @@ def collect_sensor_data(ports, data_stream):
 
     sensors_amount = 8
     repetition = 10
+
     # How many readings for each sensor do we want to receive from the device
     data_size = sensors_amount * repetition
     wrist_data = []
@@ -186,23 +151,129 @@ def collect_sensor_data(ports, data_stream):
             sys.stdout.write('ERROR: Collect_sensor_data: unknown device')
     return wrist_data, forearm_data, t
 
-if __name__ == "__main__":
+def collect_sensor_data_parallel(ports, data_stream):
+    """Collects a data reading from the given sensor ports in parallel and returns lists for each wristband values and timestamp.
+
+    >>> collect_sensor_data([]) is None
+    True
+    """
+    if not len(ports) > 0:
+        return None
+
+    sensors_amount = 8
+    repetition = 10
+
+
+
+    # How many readings for each sensor do we want to receive from the device
+    data_size = sensors_amount * repetition
+    wrist_data = []
+    forearm_data = []
+
+    t = time.time()
+    data = map(lambda x: asyncio.create_task(fetch_data_async(x, data_stream)), ports)
+    
+    data = []
+
+    for port in ports:
+        #port.write('S'.encode())
+        stream = array.array("B")
+        #data_stream.fromfile(port, data_size)
+
+        temp = data_stream[:9]
+        
+        if port == "COM4":
+            # Sensor device with 7 FSR
+            # Pop index 1 as we only have 7 FSR and that value is not doing anything.
+            temp.pop(6)
+            wrist_data = temp
+        elif port == "COM3":
+            # Sensor device with 8 FSR
+            forearm_data = temp
+        else:
+            sys.stdout.write('ERROR: Collect_sensor_data: unknown device')
+    return wrist_data, forearm_data, t
+
+async def collect_sensor_data_async(ports, data_stream):
+    """Collects a data reading from the given sensor ports in parallel and returns lists for each wristband values and timestamp.
+
+    >>> collect_sensor_data([]) is None
+    True
+    """
+    if not len(ports) > 0:
+        return None
+
+    sensors_amount = 8
+    repetition = 10
+
+
+
+    # How many readings for each sensor do we want to receive from the device
+    data_size = sensors_amount * repetition
+    wrist_data = asyncio.create_task(fetch_data_async(ports[0], data_stream))
+    forearm_data = asyncio.create_task(fetch_data_async(ports[1], data_stream))
+
+    t = time.time()
+    await wrist_data
+    await forearm_data
+
+    return wrist_data, forearm_data, t
+
+def fetch_data(port, data_stream):
+    #port.write('S'.encode())
+    stream = array.array("B")
+    #data_stream.fromfile(port, data_size)
+
+    data = data_stream[:9]
+    
+    if port == "COM4":
+        # Sensor device with 7 FSR
+        # Pop index 1 as we only have 7 FSR and that value is not doing anything.
+        data.pop(6)
+    
+    return data
+    
+async def fetch_data_async(port):
+    port.write('S'.encode())
+    stream = array.array("B")
+    data_stream.fromfile(port, data_size)
+
+    data = data_stream[:9]
+    
+    if port == "COM4":
+        # Sensor device with 7 FSR
+        # Pop index 1 as we only have 7 FSR and that value is not doing anything.
+        data.pop(6)
+    
+    return data
+
+def main():
     ports = ['COM3', 'COM4']
     
     la = [
-        'collect_sensor_data(ports, data)',
-        'old_collect_sensor_data(ports, data)',
+        'collect_sensor_data(ports)',
+        'collect_sensor_data_parallel(ports)',
+        'asyncio.get_event_loop().run_until_complete(collect_sensor_data_async(ports))',
+        'asyncio.run(collect_sensor_data_async(ports))',
     ]
+    n = 10**4
 
-    n = 10**6
+    data_stream = array.array("B", [random.randint(5, 155) for _ in range(80)])
 
     for l in la:
-        break
         elapsed = timeit.timeit(
             l, 
-            setup='import array, time, datetime, random;' 
-                'from __main__ import collect_sensor_data, old_collect_sensor_data;'
-                'data = array.array("B", [random.randint(5, 155) for _ in range(80)]);'
-                'ports = ["COM3", "COM4"];', 
+            setup='import array, time, datetime, random, asyncio;'
+                'from joblib import Parallel, delayed;' 
+                'from __main__ import test, collect_sensor_data, collect_sensor_data_parallel, collect_sensor_data_async;'
+                'ports = ["COM3", "COM4"];'
+                'loop = asyncio.get_event_loop()', 
             number=n)
         print(f"{l:30}: {elapsed:20}s = 1M * {(elapsed/n)*1000000}us ")
+
+async def test(ports, data):
+    await collect_sensor_data_async(ports, data)
+    task = asyncio.create_task(collect_sensor_data_async(ports, data))
+    
+if __name__ == "__main__":
+    main()
