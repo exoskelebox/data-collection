ff80cef3bcaff99639b06878388473132443d3b4
Mikkel Jarlund <mikkel.jarlund@gmail.com>
2020-02-19 14:53:39 +0100

    Updated config

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 class Config(object):
     DEBUG = False
     TESTING = False
+    ARM = '4083140'
+    ARM_SENSORS = 8
+    WRIST = '4407090'
+    WRIST_SENSORS=7
     
 class ProductionConfig(Config):
     DEBUG = False
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    
