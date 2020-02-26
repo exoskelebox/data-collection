a0eeb6f9b8e358381a936055036a0f74f28c2462
BullDuckling <tbuhla@gmail.com>
2020-02-26 11:49:17 +0100

    Descriptions, regions and return types

--- database.py
+++ database.py
@@ -40,63 +40,11 @@ SCHEMA = {
     """,
 }
 
-
-def _config(filename='database.ini', section='postgresql'):
-    # create a parser
-    parser = ConfigParser()
-    # read config file
-    parser.read(filename)
-
-    # get section, default to postgresql
-    db = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
-    else:
-        raise Exception(
-            'Section {0} not found in the {1} file'.format(section, filename))
-
-    return db
-
-
-def setup():
-    print(""" create tables in the PostgreSQL database""")
-    conn = None
-    try:
-        conn = _connect()
-        cur = conn.cursor()
-        # create table one by one
-        for command in SCHEMA.values():
-            cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
-        print('Exoskelebox setup complete.')
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-
-
-def _connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read database configuration
-        params = _config()
-        # connect to the PostgreSQL database
-        conn = psycopg2.connect(**params)
-        return conn
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-        raise error
-
-
-def _insert(sql: str, data, returning: bool = False):
-    """ insert data into table """
+#region INSERT handling
+def _insert(sql: str, data, returning: bool = False) -> list:
+    """ 
+    Insert data into table.
+    """
     conn = None
     returned = None
     try:
@@ -123,9 +71,10 @@ def _insert(sql: str, data, returning: bool = False):
 
     return returned
 
-
-def insert_subject(subject):
-    """ insert a new subject into the subjects table """
+def insert_subject(subject) -> int:
+    """ 
+    Insert a new subject into the subjects table. 
+    """
     columns, values = subject.keys(), subject.values()
     returning = f""
 
@@ -137,9 +86,10 @@ def insert_subject(subject):
         f"Inserted {values} for into 'subjects'. Received {subject_id} back.")
     return subject_id
 
-
-def insert_calibration(calibration):
-    """ insert a new calibration into the calibration table """
+def insert_calibration(calibration) -> None:
+    """ 
+    Insert a new calibration into the calibration table.
+    """
     columns, values = calibration.keys(), calibration.values()
 
     sql = f"""INSERT INTO calibration({','.join(columns)}) VALUES ({','.join(['%s' for _ in values])}) 
@@ -152,11 +102,12 @@ def insert_calibration(calibration):
     print(f"Inserted {values} for into 'calibration'.")
     return
 
-
-def insert_data(data):
-    """ insert new data into the data table"""
-
+def insert_data(data) -> None:
+    """ 
+    Insert new data point into the data table.
+    """
     columns, values = data.keys(), data.values()
+
     sql = f"""INSERT INTO data({','.join(columns)}) VALUES ({','.join(['%s' for _ in values])}) 
     ON CONFLICT (subject_id, gesture, repetition, reading_count) DO NOTHING
     ;"""
@@ -166,9 +117,49 @@ def insert_data(data):
     #print(f"Inserted {values} for into 'data'.")
     return
 
+def _insert_many(sql, values) -> None:
+    """ 
+    Insert lots of data into table.
+    """
+    conn = None
+    try:
+        conn = _connect()
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the sql statement
+        execute_values(cur, sql, values)
 
-def _delete(table: str, condition: str, args):
-    """ delete rows from table where condition matches """
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        raise error
+    finally:
+        if conn is not None:
+            conn.close()
+    return 
+
+def insert_data_repetition(repetition_data) -> None:
+    """
+    Insert all data from repetition.
+    """
+    start = time.perf_counter()
+    columns = repetition_data[0].keys()
+    values = [(*x.values(),) for x in repetition_data]
+    sql = f"""INSERT INTO data({','.join(columns)}) VALUES %s
+    ON CONFLICT (subject_id, gesture, repetition, reading_count) DO NOTHING
+    ;"""
+    _insert_many(sql, values)
+    elapsed = time.perf_counter() - start
+    print(f'Inserted {len(values)} data points in {elapsed} seconds')
+#endregion
+
+#region DELETE handling
+def _delete(table: str, condition: str, args) -> int:
+    """ 
+    Delete rows from table where condition matches.
+    """
     conn = None
     try:
         conn = _connect()
@@ -190,23 +181,43 @@ def _delete(table: str, condition: str, args):
         if conn is not None:
             conn.close()
 
+def delete_subject(subject_id) -> int:
+    """ 
+    Delete subject with matching id from subjects table.
+    """
+    return _delete("subjects", "(subject_id = %s)", (subject_id,))
 
-def delete_subject(subject_id):
-    _delete("subjects", "(subject_id = %s)", (subject_id,))
+def delete_calibration(subject_id, gesture) -> int:
+    """
+    Delete calibration.
+    """
+    return _delete("calibration", "(subject_id = %s AND calibration_gesture = %s)", 
+                (subject_id, gesture,))
 
+def delete_data(subject_id, gesture, repetition) -> int:
+    """
+    Delete all subjects data for relevant gesture and repetition.
+    """
+    return _delete("data", "(subject_id = %s AND gesture = %s AND repetition = %s)",
+                (subject_id, gesture, repetition,))
 
-def delete_calibration(subject_id, gesture):
-    _delete("calibration", "(subject_id = %s AND calibration_gesture = %s)",
-            (subject_id, gesture,))
+def clear_existing_data(metadata) -> int:
+    """
+    Remove data with matching metadata.
+    """
+    rows_deleted = 0
+    if exists('data', metadata):
+        rows_deleted = delete_data(
+            subject_id=metadata['subject_id'], gesture=metadata['gesture'],  repetition=metadata['repetition'])
+        print(f"Deleted {rows_deleted} rows from data for {metadata}")
+    return rows_deleted
+#endregion
 
-
-def delete_data(subject_id, gesture, repetition):
-    _delete("data", "(subject_id = %s AND gesture = %s AND repetition = %s)",
-            (subject_id, gesture, repetition,))
-
-
+#region SELECT handling
 def get_all(table):
-    """ query all data from the table """
+    """ 
+    Get all rows from table.
+    """
     conn = None
     try:
         conn = _connect()
@@ -226,86 +237,10 @@ def get_all(table):
         if conn is not None:
             conn.close()
 
-
-def _insert_dummy_subject():
-    subject = {
-        'subject_gender': 'm',
-        'subject_age': 100,
-        'subject_fitness': 1,
-        'subject_handedness': 'r',
-        'subject_impairment': 'n',
-        'subject_wrist_circumference': 20,
-        'subject_forearm_circumference': 29,
-    }
-    subject_id = insert_subject(subject)
-    return subject_id
-
-
-def _insert_dummy_data(sid):
-    data = {
-        'subject_id': sid,
-        'gesture': "dummy gesture",
-        'repetition': 1,
-        'reading_count': 1,
-        'timestamp': datetime.datetime.utcnow().strftime('%H:%M:%S.%f'),
-        'readings': [random.randint(5, 155) for _ in range(15)],
-    }
-    insert_data(data)
-
-
-def _insert_dummy_calibrations(sid):
-    for i in range(2):
-        calibration = {
-            'subject_id': sid,
-            'calibration_gesture': f"dummy gesture {i+1}",
-            'calibration_iterations': random.randint(0, 125),
-            'calibration_values': [random.randint(5, 155) for _ in range(8-i)],
-        }
-        insert_calibration(calibration)
-
-
-def insert_dummies():
-    sid = _insert_dummy_subject()
-    print(f"sid = {sid}")
-    _insert_dummy_calibrations(sid)
-    _insert_dummy_data(sid)
-
-
-def reset(table: str, cascade: bool = False):
-    """ drop table and recreate """
-    if table in SCHEMA.keys():
-        conn = None
-        try:
-            conn = _connect()
-            cur = conn.cursor()
-            sql = f"DROP TABLE IF EXISTS {table}{' CASCADE' if cascade else ''};"
-            cur.execute(sql)
-            conn.commit()
-            cur.close()
-            print(f"Dropped table '{table}'")
-
-            cur = conn.cursor()
-            cur.execute(SCHEMA[table])
-            conn.commit()
-            cur.close()
-        except (Exception, psycopg2.DatabaseError) as error:
-            print(error)
-        finally:
-            if conn is not None:
-                conn.close()
-    else:
-        print(f"Error: '{table}' does not exist")
-
-
-def reset_all():
-    print('resetting everything')
-    reset('calibration')
-    reset('data')
-    reset('subjects', cascade=True)
-    print('reset done')
-
-
-def exists(table, context):
+def exists(table, context) -> bool:
+    """
+    Checks if table contains any rows matching the context
+    """
     columns, values = context.keys(), context.values()
     where = ' AND '.join(['='.join([column, '%s']) for column in columns])
     sql = f"SELECT EXISTS(SELECT 1 FROM {table} WHERE ({where}));"
@@ -328,52 +263,108 @@ def exists(table, context):
             conn.close()
 
     return exists
+#endregion
 
+#region Utility
+def _config(filename='database.ini', section='postgresql') -> dict:
+    """ 
+    Read and parse database configuration from file.
+    """
+    # create a parser
+    parser = ConfigParser()
+    # read config file
+    parser.read(filename)
 
-def clear_existing_data(meta_data):
-    if exists('data', meta_data):
-        row_deleted = delete_data(
-            subject_id=meta_data['subject_id'], gesture=meta_data['gesture'],  repetition=meta_data['repetition'])
-        print(f"Deleted {row_deleted} rows from data for {meta_data}")
-    return
+    # get section, default to postgresql
+    db = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            db[param[0]] = param[1]
+    else:
+        raise Exception(
+            'Section {0} not found in the {1} file'.format(section, filename))
 
+    return db
 
-def _insert_many(sql, values):
-    """ insert lots of data into table """
+def setup(schema=SCHEMA):
+    """ 
+    Create tables in the PostgreSQL database.
+    """
     conn = None
-    returned = None
     try:
         conn = _connect()
-        # create a new cursor
         cur = conn.cursor()
-        # execute the INSERT statement
-        execute_values(cur, sql, values)
-
-        # commit the changes to the database
-        conn.commit()
-        # close communication with the database
+        # create table one by one
+        for command in schema.values():
+            cur.execute(command)
+        # close communication with the PostgreSQL database server
         cur.close()
+        # commit the changes
+        conn.commit()
+        print('Exoskelebox setup complete.')
     except (Exception, psycopg2.DatabaseError) as error:
-        raise error
+        print(error)
     finally:
         if conn is not None:
             conn.close()
-    return returned
+
+def _connect(config=_config()):
+    """ 
+    Connect to the database server.
+    """
+    conn = None
+    try:
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**config)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        raise error
+    return conn
+
+def reset(table: str, cascade: bool = False) -> None:
+    """ 
+    Drop table and recreate.
+    """
+    if table in SCHEMA.keys():
+        conn = None
+        try:
+            conn = _connect()
+            cur = conn.cursor()
+            sql = f"DROP TABLE IF EXISTS {table}{' CASCADE' if cascade else ''};"
+            cur.execute(sql)
+            conn.commit()
+            cur.close()
+            print(f"Dropped table '{table}'")
+
+            cur = conn.cursor()
+            cur.execute(SCHEMA[table])
+            print(f"Recreated table '{table}'")
+            conn.commit()
+            cur.close()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+        finally:
+            if conn is not None:
+                conn.close()
+    else:
+        print(f"Error: '{table}' not in schema.")
+
+def reset_all() -> None:
+    """ 
+    Drop all tables and recreate.
+    """
+    print('Resetting everything')
+
+    reset('calibration')
+    reset('data')
+    reset('subjects', cascade=True)
+
+    print('Reset done')
+#endregion
 
 
-def insert_data_repetition(repetition_data):
-    start = time.perf_counter()
-    columns = repetition_data[0].keys()
-    values = [(*x.values(),) for x in repetition_data]
-    sql = f"""INSERT INTO data({','.join(columns)}) VALUES %s
-    ON CONFLICT (subject_id, gesture, repetition, reading_count) DO NOTHING
-    ;"""
-    _insert_many(sql, values)
-    elapsed = time.perf_counter() - start
-    print(f'Inserted {len(values)} data points in {elapsed} seconds')
-
-
-def test_data_insert(n=500*10):
+def test_data_insert(n=500*10) -> None:
     print('Generating data')
     data = []
     for i in range(n):
@@ -389,6 +380,47 @@ def test_data_insert(n=500*10):
     print('Inserting data')
     insert_data_repetition(data)
 
+#region Dummy data insertion
+def _insert_dummy_subject():
+    subject = {
+        'subject_gender': 'm',
+        'subject_age': 100,
+        'subject_fitness': 1,
+        'subject_handedness': 'r',
+        'subject_impairment': 'n',
+        'subject_wrist_circumference': 20,
+        'subject_forearm_circumference': 29,
+    }
+    subject_id = insert_subject(subject)
+    return subject_id
+
+def _insert_dummy_data(sid):
+    data = {
+        'subject_id': sid,
+        'gesture': "dummy gesture",
+        'repetition': 1,
+        'reading_count': 1,
+        'timestamp': datetime.datetime.utcnow().strftime('%H:%M:%S.%f'),
+        'readings': [random.randint(5, 155) for _ in range(15)],
+    }
+    insert_data(data)
+
+def _insert_dummy_calibrations(sid):
+    for i in range(2):
+        calibration = {
+            'subject_id': sid,
+            'calibration_gesture': f"dummy gesture {i+1}",
+            'calibration_iterations': random.randint(0, 125),
+            'calibration_values': [random.randint(5, 155) for _ in range(8-i)],
+        }
+        insert_calibration(calibration)
+
+def _insert_dummies():
+    sid = _insert_dummy_subject()
+    print(f"sid = {sid}")
+    _insert_dummy_calibrations(sid)
+    _insert_dummy_data(sid)
+#endregion
 
 if __name__ == '__main__':
     setup()
