ac3f9a9f4a76364d149b50a19855b3fe22da74ad
BullDuckling <tbuhla@gmail.com>
2020-03-11 13:21:42 +0100

    tf data integration

---
+++ tf_data.py
@@ -0,0 +1,170 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow import feature_column
+from sklearn.model_selection import train_test_split
+import database as db
+import typing
+from typing import List, Tuple
+import types
+
+# A utility method to identify test data
+def is_test(x) -> bool:
+    return x['reading_count'] % 5 == 0
+# A utility method to identify training data
+def is_training(x) -> bool:
+    return not is_test(x)
+
+# A utility method to create a feature column
+# and to transform a batch of data
+def demo(feature_column, example_batch) -> None:
+    feature_layer = tf.keras.layers.DenseFeatures(feature_column)
+    print(feature_column.name)
+    print(feature_layer(example_batch).numpy())
+
+feature_column_constructors = {
+    'subject_id': None, # TODO: Decide if this should be included
+    'subject_gender': lambda x=None: get_indicator_column('subject_gender', ['m', 'f'], x),
+    'subject_age': lambda x=None: get_bucketized_column('subject_age', [18, 25, 30, 35, 40, 45, 50, 55, 60, 65], tf.uint8, x),
+    'subject_fitness': lambda x=None: get_bucketized_column('subject_fitness', [2, 4, 6, 8], tf.uint8, x),
+    'subject_handedness': lambda x=None: get_indicator_column('subject_handedness', ['l', 'r', 'a'], x),
+    'subject_impairment': lambda x=None: get_indicator_column('subject_impairment', ['True', 'False'], x),
+    'subject_wrist_circumference': lambda x=None: get_numeric_column('subject_wrist_circumference', tf.float32, x),
+    'subject_forearm_circumference': lambda x=None: get_numeric_column('subject_forearm_circumference', tf.float32, x),
+    'gesture': None, # TODO: Handle Label
+    'repetition': lambda x=None: get_numeric_column('repetition', tf.uint8, x),
+    'reading_count': lambda x=None: get_numeric_column('reading_count', tf.uint16, x),
+    'timestamp': None, # TODO: Handle timestamps
+    'readings': lambda x=None: get_numeric_array_column('readings', 15, tf.uint8, x), 
+    'arm_calibration_gesture': None, # TODO: Decide if this should be included
+    'arm_calibration_iterations': lambda x=None: get_numeric_column('arm_calibration_iterations', tf.uint16, x),
+    'arm_calibration_values': lambda x=None: get_numeric_array_column('arm_calibration_values', 8, tf.uint8, x),
+    'wrist_calibration_gesture': None, # TODO: Decide if this should be included
+    'wrist_calibration_iterations': lambda x=None: get_numeric_column('wrist_calibration_iterations', tf.uint16, x),
+    'wrist_calibration_values': lambda x=None: get_numeric_array_column('wrist_calibration_values', 7, tf.uint8, x),
+}
+GESTURE_SET = []
+def get_feature_columns(example_batch) -> List['feature_column']:
+    print(example_batch)
+    columns = []
+    for name, constructor in feature_column_constructors.items():
+        if constructor is not None:
+            columns.append(constructor(example_batch))
+    return columns
+
+def get_categorical_column_with_identity(name, example_batch=None):
+    column = feature_column.categorical_column_with_identity(name, 2)
+    column_one_hot = feature_column.indicator_column(column)
+    if example_batch:
+        demo(column_one_hot, example_batch) 
+    return column_one_hot
+
+def get_numeric_column(name, dtype=tf.float32, example_batch=None) -> feature_column.numeric_column:
+    column = feature_column.numeric_column(name, dtype=dtype)
+    if example_batch:
+        demo(column, example_batch) 
+    return column
+
+def get_numeric_array_column(name, shape, dtype=tf.float32, example_batch=None) -> feature_column.numeric_column:
+    column = feature_column.numeric_column(name, shape=shape, dtype=dtype)
+    if example_batch:
+        demo(column, example_batch) 
+    return column
+
+def get_bucketized_column(name, bucket_boundaries, dtype=tf.float32, example_batch=None) -> feature_column.bucketized_column:
+    column = feature_column.numeric_column(name, dtype=dtype)
+    bucketized_column = feature_column.bucketized_column(column, boundaries=bucket_boundaries)
+    if example_batch:
+        demo(bucketized_column, example_batch) 
+    return bucketized_column
+
+def get_indicator_column(name, vocabulary, example_batch=None) -> feature_column.indicator_column:
+    column = feature_column.categorical_column_with_vocabulary_list(name, vocabulary)
+    column_one_hot = feature_column.indicator_column(column)
+    if example_batch:
+        demo(column_one_hot, example_batch) 
+    return column_one_hot
+
+def get_data(timestamps=False) -> Tuple[tf.data.Dataset]: 
+    """
+    Retreives data from database. If timestamps = False (default) will discard timestamps.
+    Returns (train, test) tf.data.Datasets.
+    """
+    data = db.get_all('training', True)
+
+    print(len(data))
+    if len(data) > 5:
+        print(data[0])
+    else: 
+        print(data)
+
+    data_batched = {k:[d[k] for d in data] for k in data[0] if timestamps or k != 'timestamp'} # discards timestamps if timestamps = False
+
+    # Convert timestamps to iso format
+    if 'timestamp' in data_batched.keys():
+        print(data_batched['timestamp'][:5])
+        data_batched['timestamp'] = list(map(lambda x: x.isoformat(), data_batched['timestamp']))
+        print(data_batched['timestamp'][:5])
+
+    # Convert boolean values to strings
+    if 'subject_impairment' in data_batched:
+        print(data_batched['subject_impairment'][:5])
+        data_batched['subject_impairment'] = list(map(str, data_batched['subject_impairment']))
+        print(data_batched['subject_impairment'][:5])
+
+
+    tf_data = tf.data.Dataset.from_tensor_slices(data_batched)
+    print(len(list(tf_data)))
+    #print(list(tf_data))
+
+    print('Filtering:')
+    train = tf_data.filter(is_training)
+    test = tf_data.filter(is_test)
+
+    print(f'{len(list(train))}/{len(list(test))}')
+
+    return train, test
+
+def run_model(train, test) -> None:
+    
+    batch_size = 5 # A small batch sized is used for demonstration purposes
+    train = train.shuffle(buffer_size=len(list(train))).batch(batch_size)
+    test = test.shuffle(buffer_size=len(list(test))).batch(batch_size)
+    
+    # We will use this batch to demonstrate several types of feature columns
+    example_batch = next(iter(train))
+
+    feature_columns = get_feature_columns(example_batch)
+    return
+    feature_layer = tf.keras.layers.DenseFeatures(feature_columns)
+
+
+    model = tf.keras.Sequential([
+        feature_layer,
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(12, activation='softmax')
+    ])
+    
+    model.compile(optimizer='adam',
+                loss='categorical_crossentropy',
+                metrics=['accuracy'])
+
+    earlystop_callback = tf.keras.callbacks.EarlyStopping(
+    monitor='val_accuracy', min_delta=0.0001,
+    patience=3)
+
+    model.fit(train_ds,
+            validation_data=val_ds,
+            epochs=20,
+            callbacks=[earlystop_callback])
+
+    result = model.evaluate(test_ds)
+    print(result)
+
+def main():
+    train, test = get_data()
+    run_model(train, test)
+if __name__ == "__main__":
+    main()
